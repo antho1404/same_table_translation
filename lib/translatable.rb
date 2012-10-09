9e8ea7b4ef7068d0e16f86722fe16df74f972593
antho1404 <anthony.estebe@gmail.com>
2012-10-09 18:31:22 +0200

    add translatable module

---
+++ lib/translatable.rb
@@ -0,0 +1,42 @@
+require "translatable/version"
+
+module Translatable
+  def translatable(*attributes)
+    locales = I18n.available_locales
+    attributes.each do |attribute|
+      locales.each do |locale|
+        name = "#{attribute.to_s}_#{locale.to_s}"
+        attr_accessible name.to_sym if self.attribute_names.include?(name)
+      end
+    end
+
+    attributes.each do |attribute|
+      getter = (attribute.to_s).to_sym
+      setter = ("#{attribute.to_s}=").to_sym
+      
+      send :define_method, getter do
+        begin
+          self.attributes["#{attribute.to_s}_#{I18n.locale.to_s}"]
+        rescue e
+        end
+      end
+
+      send :define_method, setter do |value|
+        begin
+          self.attributes = { "#{attribute}_#{I18n.locale.to_s}" => value }
+        rescue
+        end
+      end
+    end
+
+    scope :translated, lambda { |attribute|
+      where(arel_table["#{attribute.to_s}_#{I18n.locale.to_s}".to_sym].not_eq(nil))
+    }
+
+    scope :untranslated, lambda { |attribute|
+      where(arel_table["#{attribute.to_s}_#{I18n.locale.to_s}".to_sym].eq(nil))
+    }
+  end
+end
+
+ActiveRecord::Base.extend Translatable
---
+++ translatable.gemspec
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'translatable/version'
+
+Gem::Specification.new do |gem|
+  gem.name          = "translatable"
+  gem.version       = Translatable::VERSION
+  gem.authors       = ["antho1404"]
+  gem.email         = ["anthony.estebe@gmail.com"]
+  gem.description   = %q{Translate a simple attribute in all your locales }
+  gem.summary       = %q{This is similar to most of other translation plugin in rails but usefull if you want to translate in few language because we don't need any join like others plugins. }
+  gem.homepage      = ""
+
+  gem.files         = `git ls-files`.split($/)
+  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ["lib"]
+end
